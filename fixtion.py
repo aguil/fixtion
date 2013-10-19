562137c9bfd2faa9be8a4346e000d73212437a8e
Jason Aguilon <jason.aguilon@webfilings.com>
2013-10-18 22:15:20 -0400

    Passing doctests in README examples

--- fixtion.py
+++ fixtion.py
@@ -1,12 +1,9 @@
-from functools import wraps
 import functools
+from functools import wraps
+import sys
 import inspect
 from itertools import chain
 
-import mock
-
-from contextdecorator import contextmanager, GeneratorContextManager, ContextDecorator
-import sys
 from contextlib_extras import ExitStack
 
 
@@ -18,8 +15,9 @@ def _reraise(cls, val, tb):
 
 
 class FixtureDecorator(object):
-    "A base class or mixin that enables context managers to work as decorators."
-
+    """A base class or mixin that enables fixture function context managers to
+        work as decorators.
+    """
     def __call__(self, f):
         @wraps(f)
         def inner(*args, **kw):
@@ -41,17 +39,13 @@ class FixtureDecorator(object):
 
 
 class GeneratorFixtureManager(FixtureDecorator):
-    """Helper for @contextmanager decorator."""
-
+    """Helper for @fixture decorator."""
     def __init__(self, func, gen, *args, **kwargs):
         self.func_gen = gen
         self.args = args
         self.kwargs = kwargs
         self.func = func
         self.stack = ExitStack()
-#        if hasattr(self.func, '__test__'):
-#        self.wrapper.__test__ = True
-        # functools.update_wrapper(self.wrapper, func)
 
     def __get__(self, obj, type=None):
         if obj:
@@ -66,15 +60,15 @@ class GeneratorFixtureManager(FixtureDecorator):
     def __call__(self, obj=None):
         context = self.__enter__()
 
-#        obj = getattr(self.func, 'im_self', None)
-
         exc = _NO_EXCEPTION
+        result = None
         try:
             args = tuple(filter(None, (obj, context)))
-            result = self.func(*args)
+            result = (self.func
+                        and self.func(*args)
+                        or None)
         except Exception:
             exc = sys.exc_info()
-            result = None
 
         catch = self.__exit__(*exc)
 
@@ -84,10 +78,13 @@ class GeneratorFixtureManager(FixtureDecorator):
         return result
 
     def __enter__(self):
-    #        patchers = tuple(self.stack.enter_context(patch)
-    #                         for patch in reversed(getattr(self.gen, 'patchings', [])))
-    #        self.gen = self.gen(*chain(patchers, self.args), **self.kwargs)
-        self.gen = self.func_gen(*self.args, **self.kwargs)
+        fixture_patchings = reversed(
+            getattr(self.func_gen, 'fixture_patchings', []))
+
+        patchers = tuple(
+            self.stack.enter_context(patch) for patch in fixture_patchings)
+
+        self.gen = self.func_gen(*chain(patchers, self.args), **self.kwargs)
 
         try:
             context = next(self.gen)
@@ -143,7 +140,8 @@ def decorator(func):
     if isinstance(func, type):
         def class_wrapper(*args, **kw):
             if isFuncArg(*args, **kw):
-                return func()(*args, **kw) # create class before usage
+                inst = func()
+                return inst(*args, **kw) # create class before usage
             return func(*args, **kw)
 
         class_wrapper.__name__ = func.__name__
@@ -155,7 +153,7 @@ def decorator(func):
         if isFuncArg(*args, **kw):
             return func(*args, **kw)
 
-        def functor(userFunc):
+        def functor(userFunc=None):
             return func(userFunc, *args, **kw)
 
         return functor
@@ -165,174 +163,12 @@ def decorator(func):
 
 @decorator
 def fixture(func):
-#    @wraps(func)
+    @wraps(func)
     @decorator
     def wrapper(test_func, *args, **kwds):
-        functools.update_wrapper(wrapper, test_func)
+        if test_func:
+            functools.update_wrapper(wrapper, test_func)
         return GeneratorFixtureManager(test_func, func, *args, **kwds)
 
-    #    func.fixture_patchings, func.patchings = getattr(func, 'patchings', []), []
+    func.fixture_patchings, func.patchings = getattr(func, 'patchings', []), []
     return wrapper
-
-
-def patch(target, new=mock.DEFAULT, spec=None, create=False,
-          spec_set=None, autospec=None, new_callable=None, **kwargs):
-    getter, attribute = mock._get_target(target)
-    return _patch(
-        getter, attribute, new, spec, create,
-        spec_set, autospec, new_callable, kwargs)
-
-
-class _patch(mock._patch):
-    def decorate_callable(self, func):
-        if hasattr(func, 'patchings'):
-            func.patchings.append(self)
-            return func
-
-        @wraps(func)
-        def patched(*args, **keywargs):
-            return patcher(func, patched.patchings, *args, **keywargs)
-
-        patched.patchings = [self]
-        if hasattr(func, 'func_code'):
-            # not in Python 3
-            patched.compat_co_firstlineno = getattr(
-                func, "compat_co_firstlineno",
-                func.func_code.co_firstlineno
-            )
-        return patched
-
-
-#@contextmanager
-def patcher(func, patchings, *args, **keywargs):
-    # don't use a with here (backwards compatability with Python 2.4)
-    extra_args = []
-    entered_patchers = []
-
-    def stop_patchers():
-        for patching in reversed(entered_patchers):
-            patching.__exit__(*exc_info)
-
-    # can't use try...except...finally because of Python 2.4
-    # compatibility
-    exc_info = tuple()
-    patching = None
-    try:
-        try:
-            for patching in patchings:
-                arg = patching.__enter__()
-                entered_patchers.append(patching)
-                if patching.attribute_name is not None:
-                    keywargs.update(arg)
-                elif patching.new is mock.DEFAULT:
-                    extra_args.append(arg)
-
-            args += tuple(extra_args)
-            gen = func(*args, **keywargs)
-
-        except:
-            if (patching not in entered_patchers and
-                mock._is_started(patching)):
-                # the patcher may have been started, but an exception
-                # raised whilst entering one of its additional_patchers
-                entered_patchers.append(patching)
-                # Pass the exception to __exit__
-            exc_info = sys.exc_info()
-            # re-raise the exception
-            raise
-
-    finally:
-        stop_patchers()
-
-    yield next(gen)
-
-    next(gen)
-
-    stop_patchers()
-
-#@fixture
-#@mock.patch('logging.error')
-#def myfixture(a_mock):
-#    print repr(a_mock)
-#    print 'before'
-#    yield
-#    print 'after'
-#
-#
-#@myfixture
-#def test():
-#    print 'tada'
-
-
-@fixture
-def basic_fixture():
-    print 'before basic'
-    yield
-    print 'after basic'
-
-
-@basic_fixture
-def test_basic():
-    print 'tada - test_basic'
-
-
-@fixture
-def a_fixture(a):
-    print 'before a: %r' % a
-    yield
-    print 'after a: %r' % a
-
-
-@a_fixture(42)
-def test_with_a():
-    print 'tada - test_with_a'
-
-
-@fixture
-def b_fixture():
-    print 'before b'
-    yield 42
-    print 'after b'
-
-
-@b_fixture
-def test_with_b(context):
-    print 'tada - test_with_b: %r' % context
-
-
-import logging
-
-
-@fixture
-@patch('logging.error')
-def c_fixture(logging_mock):
-    print 'before c: %r' % logging.error
-    yield
-    print 'after c: %r' % logging.error
-
-
-@c_fixture
-def test_with_c():
-    print 'tada - test_with_c'
-
-
-@fixture
-@patch('logging.info')
-def example_fixture(logging_mock):
-    print 'enter example_fixture: %r' % logging.error
-    yield 123
-    print 'exit example_fixture: %r' % logging.error
-
-
-@example_fixture
-def test_using_example_fixture(context):
-    print '  tada - test_using_example_fixture'
-    print '  context: %r' % context
-
-
-if __name__ == '__main__':
-    test_basic()
-    test_with_a()
-    test_with_b()
-    test_with_c()
-    test_using_example_fixture()
--- test_fixtion.py
+++ test_fixtion.py
@@ -1,100 +1,25 @@
-from types import FunctionType
 import unittest
+from unittest import skip
 
 import mock
 
-from fixtion import fixture, basic_fixture
-
-
-something = mock.sentinel.Something
-something_else = mock.sentinel.SomethingElse
+from fixtion import fixture
 
 
 class Foo(object):
     pass
 
 
-def bar():
-    pass
-
-
-#class FixtureContextDecorator_tests(unittest.TestCase):
-#    def test_returns_no_context(self):
-#        class myfixture(FixtureContextDecorator):
-#            def __enter__(self):
-#                pass
-#
-#            def __exit__(self, *exc):
-#                pass
-#
-#        myfixture = myfixture()
-#
-#        @myfixture
-#        def test(context):
-#            self.assertEqual(None, context)
-#
-#        test()
-#
-#    def test_returns_context(self):
-#        class myfixture(FixtureContextDecorator):
-#            def __enter__(self):
-#                return 42
-#
-#            def __exit__(self, *exc):
-#                pass
-#
-#        myfixture = myfixture()
-#
-#        @myfixture
-#        def test(context):
-#            self.assertEqual(42, context)
-#
-#        test()
-#
-#    def test_returns_self(self):
-#        class myfixture(FixtureContextDecorator):
-#            def __enter__(self):
-#                return self
-#
-#            def __exit__(self, *exc):
-#                pass
-#
-#        myfixture = myfixture()
-#
-#        @myfixture
-#        def test(context):
-#            self.assertIs(myfixture, context)
-#
-#        test()
-#
-#    def test_with_mock(self):
-#        class myfixture(FixtureContextDecorator):
-#            def __enter__(self):
-#                return self
-#
-#            def __exit__(self, *exc):
-#                pass
-#
-#        myfixture = myfixture()
-#
-#        @mock.patch('%s.bar' % __name__)
-#        @mock.patch('%s.Foo' % __name__)
-#        @myfixture
-#        def test(foo_mock, bar_mock, context):
-#            self.assertIsInstance(foo_mock, mock.Mock)
-#            self.assertEqual('Foo', foo_mock._mock_name)
-#
-#            self.assertIsInstance(bar_mock, mock.Mock)
-#            self.assertEqual('bar', bar_mock._mock_name)
-#
-#            self.assertIs(myfixture, context)
-#
-#        test()
-
-
 @fixture
 def basic_fixture():
-    yield
+    class Context(object):
+        def __init__(self):
+            self.enter = mock.Mock()
+            self.exit = mock.Mock()
+    context = Context()
+    context.enter()
+    yield context
+    context.exit()
 
 
 @fixture
@@ -105,93 +30,238 @@ def patched_fixture(randint):
     yield context()
 
 
-mock_function = mock.MagicMock(spec=FunctionType, __test__=False)
-mock_fixture = fixture(mock_function)
+@fixture
+def patched_dict_fixture(**kwargs):
+    with mock.patch.dict('os.environ', kwargs):
+        yield
+
+
+@fixture
+@mock.patch.dict('os.environ', foo='bar')
+def patched_dict_decorated_fixture():
+    yield
 
 
 class basic_fixture_tester(unittest.TestCase):
-    @mock_fixture
-    def test_basic_fixture(self):
-        mock_function.assert_called_once()
+    @basic_fixture
+    def test_fixture_entered(self, context):
+        context.enter.assert_called_once_with()
+
+    def test_fixture_entered(self):
+        @basic_fixture
+        def foo(context):
+            return context
+        foo().enter.assert_called_once_with()
+
+    def test_foo_fixture_as_context_manager(self):
+        # TODO: Rid this ()() ugliness.
+        with basic_fixture()():
+            pass
 
 
 class patched_fixture_tester(unittest.TestCase):
     @patched_fixture
     def test_patched_fixture(self, context):
-        self.assertTrue(False)
-        self.assertIsInstance(context.randint_stub, unittest.TestCase)
+        self.assertIsInstance(context.randint_stub, mock.MagicMock)
 
-#class fixture_function_tests(unittest.TestCase):
-#    def test_basic(self):
-#        @fixture
-#        def some_fixture():
-#            class context(object):
-#                foo = 42
-#            yield context
-#
-#        @some_fixture
-#        def test(context):
-#            self.assertEqual(42, context.foo)
-#
-#        test()
-#
-#    def test_with_arg(self):
-#        @fixture
-#        def some_fixture(a):
-#            yield a
-#
-#        @some_fixture(params=(42,))
-#        def test(context):
-#            self.assertEqual(42, context)
-#
-#        test()
-#
-#    def test_with_mock(self):
-#        Foo_original = Foo
-#
-#        @fixture(patchings=(mock.patch('%s.Foo' % __name__),))
-#        def some_fixture(foo_mock):
-#            self.assertIsInstance(foo_mock, mock.Mock)
-#            self.assertEqual('Foo', foo_mock._mock_name)
-#
-#            class context(object):
-#                foo = 42
-#
-#            yield context
-#
-#            self.assertIsInstance(foo_mock, mock.Mock)
-#            self.assertEqual('Foo', foo_mock._mock_name)
-#
-#        @some_fixture
-#        def test(context):
-#            self.assertEqual(42, context.foo)
-#            self.assertIsInstance(Foo, mock.Mock)
-#
-#        test()
-#        self.assertIs(Foo, Foo_original)
-#
-#    def test_with_arg_and_mock(self):
-#        Foo_original = Foo
-#
-##        @fixture(patchings=(mock.patch('%s.Foo' % __name__),))
-#        @mock.patch('%s.Foo' % __name__)
-#        @fixture
-#        def some_fixture(a, foo_mock):
-#            self.assertIsInstance(foo_mock, mock.Mock)
-#            self.assertEqual('Foo', foo_mock._mock_name)
-#
-#            class context(object):
-#                foo = 42
-#
-#            yield context
-#
-#            self.assertIsInstance(foo_mock, mock.Mock)
-#            self.assertEqual('Foo', foo_mock._mock_name)
-#
-#        @some_fixture(params=(42,))
-#        def test(context):
-#            self.assertEqual(42, context.foo)
-#            self.assertIsInstance(Foo, mock.Mock)
-#
-#        test()
-#        self.assertIs(Foo, Foo_original)
+
+class patched_dict_fixture_tests(unittest.TestCase):
+    @patched_dict_fixture(foo='bar')
+    def test_has_foo_value(self):
+        import os
+        self.assertEqual('bar', os.environ['foo'])
+
+    def test_removes_foo_value(self):
+        import os
+        with patched_dict_fixture(foo='bar')():
+            pass
+        self.assertNotIn('foo', os.environ)
+
+    @mock.patch.dict('os.environ', {'foo': 'buzz'})
+    def test_changes_foo_value(self):
+        import os
+        with patched_dict_fixture(foo='bar')():
+            self.assertEqual('bar', os.environ['foo'])
+
+    @mock.patch.dict('os.environ', {'foo': 'buzz'})
+    def test_restores_foo_value(self):
+        import os
+        with patched_dict_fixture(foo='bar')():
+            pass
+        self.assertEqual('buzz', os.environ['foo'])
+
+
+@skip('TODO: Make @patch.dict work')
+class patched_dict_decorated_fixture_tests(unittest.TestCase):
+    @patched_dict_decorated_fixture
+    def test_has_foo_value(self):
+        import os
+        self.assertIn('foo', os.environ)
+        self.assertEqual('bar', os.environ['foo'])
+
+    @patched_dict_decorated_fixture
+    def test_removes_foo_value(self):
+        import os
+        with patched_dict_fixture(foo='bar')():
+            pass
+        self.assertNotIn('foo', os.environ)
+
+    @mock.patch.dict('os.environ', {'foo': 'buzz'})
+    def test_changes_foo_value(self):
+        import os
+
+        actual = {}
+
+        @patched_dict_decorated_fixture
+        def test():
+            actual['foo'] = os.environ['foo']
+
+        test()
+        self.assertEqual({'foo': 'bar'}, actual)
+
+    @mock.patch.dict('os.environ', {'foo': 'buzz'})
+    def test_restores_foo_value(self):
+        import os
+
+        @patched_dict_decorated_fixture
+        def test():
+            pass
+
+        test()
+        self.assertEqual('buzz', os.environ['foo'])
+
+
+class fixture_function_tests(unittest.TestCase):
+    def test_basic(self):
+        @fixture
+        def some_fixture():
+            class context(object):
+                foo = 123
+
+            yield context
+
+        @some_fixture
+        def test(context):
+            self.assertEqual(123, context.foo)
+
+        test()
+
+    def test_with_arg(self):
+        @fixture
+        def some_fixture(a):
+            yield a
+
+        @some_fixture(123)
+        def test(context):
+            self.assertEqual(123, context)
+
+        test()
+
+    def test_with_mock_patch(self):
+        Foo_original = Foo
+
+        @fixture
+        @mock.patch('test_fixtion.Foo')
+        def some_fixture(foo_mock):
+            self.assertIsInstance(foo_mock, mock.Mock)
+            self.assertEqual('Foo', foo_mock._mock_name)
+
+            class context(object):
+                foo = 123
+
+            yield context
+
+            self.assertIsInstance(foo_mock, mock.Mock)
+            self.assertEqual('Foo', foo_mock._mock_name)
+
+        @some_fixture
+        def test(context):
+            self.assertEqual(123, context.foo)
+            self.assertIsInstance(Foo, mock.Mock)
+
+        test()
+        self.assertIs(Foo, Foo_original)
+
+    def test_with_arg_and_mock(self):
+        Foo_original = Foo
+
+        @fixture
+        @mock.patch('test_fixtion.Foo')
+        def some_fixture(foo_mock, a):
+            self.assertIsInstance(foo_mock, mock.Mock)
+            self.assertEqual('Foo', foo_mock._mock_name)
+
+            class context(object):
+                foo = a
+
+            yield context
+
+            self.assertIsInstance(foo_mock, mock.Mock)
+            self.assertEqual('Foo', foo_mock._mock_name)
+
+        @some_fixture(123)
+        def test(context):
+            self.assertEqual(123, context.foo)
+            self.assertIsInstance(Foo, mock.Mock)
+
+        test()
+        self.assertIs(Foo, Foo_original)
+
+    @skip('TODO: Compose fixtures')
+    def test_compose_fixtures(self):
+        from mock import call
+
+        context = mock.Mock()
+
+        @fixture
+        def foo_fixture():
+            context.enter_foo()
+            yield
+            context.exit_foo()
+
+        @foo_fixture
+        def foobar_fixture():
+            context.enter_bar()
+            yield
+            context.exit_bar()
+
+        @foobar_fixture
+        def test():
+            pass
+
+        test()
+
+        self.assertEqual(
+            [call.enter_foo(), call.enter_bar(), call.exit_bar(),
+             call.exit_foo()],
+            context.mock_calls)
+
+    @skip('TODO: Stack fixtures')
+    def test_stack_fixtures(self):
+        from mock import call
+
+        context = mock.Mock()
+
+        @fixture
+        def foo_fixture():
+            context.enter_foo()
+            yield
+            context.exit_foo()
+
+        @fixture
+        def bar_fixture():
+            context.enter_bar()
+            yield
+            context.exit_bar()
+
+        @foo_fixture
+        @bar_fixture
+        def test():
+            pass
+
+        test()
+
+        self.assertEqual(
+            [call.enter_foo(), call.enter_bar(), call.exit_bar(),
+             call.exit_foo()],
+            context.mock_calls)
